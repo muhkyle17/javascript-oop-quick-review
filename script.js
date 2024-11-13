4997c8df2b32eb00e5456d010958d82b41087ba1
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-13 12:16:49 -0800

    Add notes for encapsulation

--- script.js
+++ script.js
@@ -21,9 +21,18 @@ class Dog extends Animal {
 }
 
 class Cat extends Animal {
+  #weight
   constructor(name, age, weight) {
     super(name, age)
-    this.weight = weight
+    this.#weight = weight
+  }
+
+  getWeight() {
+    return this.#weight
+  }
+
+  setWeight(weight) {
+    this.#weight = weight
   }
 }
 
@@ -31,3 +40,11 @@ const dog = new Dog('Rex', 2, 'German Shepherd')
 console.log(dog.getInfo())
 console.log(dog.breed)
 console.log(dog.bark())
+
+const cat = new Cat('Whiskers', 5, '5kg')
+console.log(cat.getInfo())
+console.log(cat.weight) // undefined
+console.log(cat.getWeight()) // 5kg
+
+cat.setWeight('6kg')
+console.log(cat.getWeight()) // 6kg
